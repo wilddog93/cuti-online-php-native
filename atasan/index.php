1f42aca4b2cd9e3ba8f1c728533277b7e9db9fea
ridhoajibx <ridhoajibx@gmail.com>
2020-05-31 12:29:33 +0700

    create footer

--- atasan/index.php
+++ atasan/index.php
@@ -370,17 +370,7 @@ $isi_departemen = $ada['hasil'];
   <!-- END MAIN -->
   <!-- //////////////////////////////////////////////////////////////////////////// -->
   <!-- START FOOTER -->
-  <footer class="page-footer gradient-45deg-light-blue">
-    <div class="footer-copyright">
-      <div class="container">
-        <span>Copyright ©
-          <script type="text/javascript">
-            document.write(new Date().getFullYear());
-          </script> <a class="grey-text text-lighten-2" href="#" target="_blank">PT Angkasa Engineers Indonesia</a> All rights reserved.</span>
-        <span class="right hide-on-small-only"> Design and Developed by <a class="grey-text text-lighten-2" href="#">Jumakri Ridho Fauzi</a></span>
-      </div>
-    </div>
-  </footer>
+  <?php include_once($backurl . 'config/footer.php'); ?>
   <!-- END FOOTER -->
   <!-- ================================================
     Scripts
